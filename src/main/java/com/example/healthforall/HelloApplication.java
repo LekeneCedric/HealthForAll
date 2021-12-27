d5a70868b0625cf77a9e5d062913b102cb8ebd6e
LekeneCedric <93648451+LekeneCedric@users.noreply.github.com>
2021-12-27 13:51:49 -0800

    22:50 (27/12/21)

--- src/main/java/com/example/healthforall/HelloApplication.java
+++ src/main/java/com/example/healthforall/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signup-signin-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("HealthForAll");
+        stage.getIcons();
         stage.setScene(scene);
         stage.show();
     }
