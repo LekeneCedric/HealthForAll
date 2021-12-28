b01834c75575bd8580b706c394ce584d0f6132e2
LekeneCedric <93648451+LekeneCedric@users.noreply.github.com>
2021-12-28 00:31:55 -0800

    9 : 30  28/12/21

--- src/main/java/com/example/healthforall/HelloController.java
+++ src/main/java/com/example/healthforall/HelloController.java
@@ -1,14 +1,55 @@
 package com.example.healthforall;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TabPane signBar;
+    @FXML
+    private Label info;
+
+    @FXML
+    private Button signIn;
+
+    @FXML
+    private TextField signinPassword;
+
+    @FXML
+    private Tab signinTab;
+
+    @FXML
+    private TextField signinUsername;
+
+    @FXML
+    private TextField signupEmail;
+
+    @FXML
+    private TextField signupLastname;
+
+    @FXML
+    private TextField signupPassword;
+
+    @FXML
+    private Tab signupTab;
+
+    @FXML
+    private TextField signupUsername;
+
+    @FXML
+    private Button switchsignin;
+
+    @FXML
+    private Button switchsignup;
+    @FXML
+    protected void signIn()
+    {
+        String user;
+        user = "Bienvenue "+signinUsername.getText() +" Mot de passe : "+signinPassword.getText();
+        info.setText(user);
+        
     }
+
+
 }
