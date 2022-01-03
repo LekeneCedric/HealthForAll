195d11687cb254b2aef946c1fc3768e0f396dd24
LekeneCedric <93648451+LekeneCedric@users.noreply.github.com>
2022-01-03 13:22:00 -0800

    last modif

---
+++ src/main/java/com/example/healthforall/FxmlLoader.java
@@ -0,0 +1,30 @@
+package com.example.healthforall;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+import java.net.URI;
+import java.net.URL;
+
+public class FxmlLoader {
+    private Pane view ;
+
+    public Pane getView (String filename)
+    {
+            try {
+                    URL link =HelloApplication.class.getResource("target/classes/com/example/healthforall/hello-view"+filename+".fxml");
+                    if(link == null)
+                    {
+                        System.out.println("Erreur lors du chargement de la page");
+
+                    }
+                    view = new FXMLLoader().load(link);
+            }
+            catch(Exception e)
+            {
+               System.out.println("Erreur lors du chargement de la page");
+            }
+            return view;
+    }
+
+}
