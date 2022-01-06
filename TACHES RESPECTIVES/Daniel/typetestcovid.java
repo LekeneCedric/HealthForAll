a41e3081a1ef0f762aaf8c0248b07febf236b345
daniel iya <iyadaniel51@gmail.com>
2022-01-06 15:47:52 +0100

    les modification du test covid

--- TACHES RESPECTIVES/Daniel/typetestcovid.java
+++ TACHES RESPECTIVES/Daniel/typetestcovid.java
@@ -1,24 +1,25 @@
 public class typetestcovid {
     void afficher() {
-        System.out.println("\nLES DIFFERENTS TYPES DE TESTS\n\n\n");
+        System.out.println("\n--------------------------------------------------------------------------------------\n");
+        System.out.println("\nLES DIFFERENTS TYPES DE TESTS\n\n");
 
-        System.out.println("\n\nLes tests virologiques (RT-PCR)\n\n");
+        System.out.println("\nLes tests virologiques (RT-PCR)");
         System.out.println(
                 "Ils permettent de déterminer si une personne est porteuse du virus au moment du test, avec un prélèvement par voie nasale ou salivaire. Le résultat est en général disponible dans les 24 heures qui suivent. Afin de faciliter le dépistage de la Covid-19, les tests PCR sont totalement pris en charge par l'assurance maladie et ne nécessitent pas de prescription d'un médecin.");
 
-        System.out.println("\n\nLes tests sérologiques\n\n");
+        System.out.println("\nLes tests sérologiques");
         System.out.println(
                 "Ils permettent de rechercher si une personne a développé une réaction immunitaire après avoir été en contact avec le virus. Ces tests détectent la présence d'anticorps au moyen d'une prise de sang. Les tests sérologiques fiables figurent sur une liste en ligne. Ils sont remboursés par la sécurité sociale lorsqu’ils entrent dans le cadre des indications définies par la Haute Autorité de Santé.");
 
-        System.out.println("\n\nLes tests antigéniques\n\n");
+        System.out.println("\nLes tests antigéniques");
         System.out.println(
                 "Ils détectent la présence du virus ou de fragments de virus SARS-CoV-2. Comme les tests par RT-PCR, ils permettent le diagnostic précoce des maladies dès la phase aiguë. Ils répondent à la question « le patient est-il oui ou non porteur du Covid-19 ? ». Comme le test de référence actuel, le RT-PCR, les tests antigéniques sont à ce jour réalisés à partir de prélèvements dans le nez, par écouvillon. Mais alors que le RT-PCR nécessite une analyse parfois de plusieurs heures en laboratoire, pour détecter le matériel génétique du coronavirus, le test antigénique repère des protéines du virus en moins de 30 minutes.");
 
-        System.out.println("\n\nLes autotests nasaux\n\n");
+        System.out.println("\nLes autotests nasaux");
         System.out.println(
                 "Il s'agit de tests antigéniques dont le prélèvement et la lecture du résultat peuvent être réalisés seul, d’après les indications fournies par un professionnel et après lecture des conditions d’utilisation et du « guide d’utilisation » fournis au moment de l’achat. Le mode de prélèvement autorisé aujourd’hui est l’auto-prélèvement nasal, qui est moins profond que le prélèvement nasopharyngé pratiqué pour les tests PCR et antigéniques classiques. Le résultat est déterminé en quinze à vingt minutes, selon la notice du fabricant.");
 
-        System.out.println("\n\nLes tests salivaires\n\n");
+        System.out.println("\nLes tests salivaires");
         System.out.println(
                 "Ils correspondent à un examen de la salive ; ils ne font pas mal. Ils sont déployés dans les écoles maternelles et élémentaires pour renforcer le dépistage au Covid-19 et rompre au plus vite les chaînes de contamination. Leur prélèvement est plus acceptable par les jeunes enfants que celui du test nasopharyngé (par le nez).");
     }
