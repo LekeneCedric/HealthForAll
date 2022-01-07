2d326b1ba52ed8fa5bc2bd2117f79d3091ad90cf
LekeneCedric <93648451+LekeneCedric@users.noreply.github.com>
2022-01-06 23:32:48 -0800

    last modif

--- src/main/java/com/example/healthforall/CovidTestController.java
+++ src/main/java/com/example/healthforall/CovidTestController.java
@@ -1,4 +1,55 @@
 package com.example.healthforall;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
+
 public class CovidTestController {
+    @FXML
+    private Label nompatientcovid;
+
+    @FXML
+    private ToggleGroup symptomeFrequen2;
+
+    @FXML
+    private ToggleGroup symptomeFrequent1;
+
+    @FXML
+    private ToggleGroup symptomeFrequent2;
+
+    @FXML
+    private ToggleGroup symptomeFrequent3;
+
+    @FXML
+    private ToggleGroup symptomeFrequent4;
+
+    @FXML
+    private ToggleGroup symptomeGrave1;
+
+    @FXML
+    private ToggleGroup symptomeGrave2;
+
+    @FXML
+    private ToggleGroup symptomeGrave3;
+
+    @FXML
+    private ToggleGroup symptomeMoinsFrequent1;
+
+    @FXML
+    private ToggleGroup symptomeMoinsFrequent2;
+
+    @FXML
+    private ToggleGroup symptomeMoinsFrequent3;
+
+    @FXML
+    private ToggleGroup symptomeMoinsFrequent4;
+
+    @FXML
+    private ToggleGroup symptomeMoinsFrequent5;
+
+    @FXML
+    private ToggleGroup symptomeMoinsFrequent6;
+
+    @FXML
+    private ToggleGroup symptomeMoinsFrequent7;
 }
